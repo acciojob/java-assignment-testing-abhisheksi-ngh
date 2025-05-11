1cd4ac8d019fb6e8d697005a68b2b32a018ac620
A <abhishekkumarsingh9585@gmail.com>
2025-05-11 20:54:01 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,38 +1,41 @@
-/**
- * Main class of the Java program. 
- * 
- */
+package com.driver;
+
 import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-        //your code here
-         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+    // Yeh method testable hai
+    public static String generatePattern(int N) {
         String s = "";
         boolean toggle = false;
-        
+
         char[] alphabets = "abcdefghijklmnopqrstuvwyz".toCharArray();
-        
-        Queue<Character> q = new LinkedList();
-        
-        for(int i=0; i<alphabets.length; i++)
-            q.add(alphabets[i]);
-        
-        while(N > 0) {
+        Queue<Character> q = new LinkedList<>();
+
+        for (char c : alphabets)
+            q.add(c);
+
+        while (N > 0) {
             char ch = q.remove();
             if (toggle)
                 s = s + ch;
             else
                 s = ch + s;
-            
-            toggle = !toggle;
-            
-            q.add(ch);
-            N = N-1;
-        }
-        System.out.println(s);
 
-           }
+            toggle = !toggle;
+            q.add(ch);
+            N--;
+        }
+
+        return s;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        // ab method ko call karke output le rahe hain
+        String result = generatePattern(N);
+        System.out.println(result);
+    }
 }
